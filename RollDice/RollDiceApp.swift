acbabe42bd8a0efd459143981faddb9d890c9a0b
alexliou <alexliou0@gmail.com>
2022-06-04 15:30:20 -0400

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Alex Liou on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
